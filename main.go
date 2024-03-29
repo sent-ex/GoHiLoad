16d0fa0b49e434002edffbce773faeb6a27cccaa
Vassili Kouznetsov <wasowaso@mail.ru>
2024-03-29 17:20:07 +0700

    added "start" banner to main function

--- main.go
+++ main.go
@@ -51,6 +51,7 @@ func IntBubbleSorting(Arr *[]int) bool {
 }
 
 func main() {
+	println ("starting")
 	var ArraySize = 2000000 // будущий размер массива
 	StartTime := time.Now()
 	myArray := make([]int, 0) // создали пустой слайс под будущую сортировку
